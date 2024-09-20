b937201554c5f54a8a78eb2bd8be12d3df6bf36e
Joao Rodolfo <joaorodolforosa@gmail.com>
2024-09-20 17:58:51 -0300

    Criação Projeto e configuração H2

---
+++ src/test/java/br/uam/sdm/SdmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uam.sdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
